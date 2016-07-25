9b83922b39af0ab4b1fad049f9b5b93ae030f38c
Sebastian Lindberg <seb.lindberg@gmail.com>
2016-07-25 23:06:35 +0200

    Added test for #to_s

--- test/bucket_test.rb
+++ test/bucket_test.rb
@@ -30,6 +30,13 @@ describe Tele2::Bucket do
       assert_in_epsilon example_data['used'].to_f, bucket.used, 0.01
     end
   end
+  
+  describe '#used_percentage' do
+    it 'returns the used amount in percent' do
+      assert_in_epsilon example_data['usedPercentage'].to_f,
+        bucket.used_percentage, 0.01
+    end
+  end
 
   describe '#remaining' do
     it 'returns the remaining amount' do
@@ -40,6 +47,15 @@ describe Tele2::Bucket do
   describe '#label' do
     it 'returns the description' do
       assert_equal example_data['description'], bucket.label
+    end
+  end
+  
+  describe '#to_s' do
+    it 'returns the formatted bucket' do
+      lines = bucket.to_s.split "\n"
+      assert_equal bucket.label + ':', lines.first
+      assert_equal format('%s left of %s (%.1f%%)', bucket.remaining,
+                     bucket.total, bucket.used_percentage), lines.last
     end
   end
 end
